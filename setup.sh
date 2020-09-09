e5788e55b39ca3b8b6daa37900748ed38ed23e86
root <root@kali>
2020-09-09 08:15:38 -0400

    tmux

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
 sudo apt update
 sudo apt install sublime-text
+sudo apt-get install tmux
 git config user.email "anshulbahrania@gmail.com"
 git config user.name "Anshul-Bahrani"
