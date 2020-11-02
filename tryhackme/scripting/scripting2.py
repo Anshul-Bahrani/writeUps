5177f06d81c394f33b7e0aa6852208e72fe4a660
Anshul-Bahrani <anshulbahrania@gmail.com>
2020-11-02 21:10:30 +0000

    Scripting ontheway

--- tryhackme/scripting/scripting2.py
+++ tryhackme/scripting/scripting2.py
@@ -1,10 +1,12 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ip = "10.10.70.49"
+port = 3010
 while True:
 	try:
-		s.connect(("10.10.113.180", 1337))
-		s.send("GET / HTTP/1.0\r\n\r\n".encode())
+		s.connect((ip, port))
+		s.send(f"GET / HTTP/1.0\r\n\r\nHost: {ip}: {port}".encode())
 		print(s.recv(4096).decode())
 	except ConnectionRefusedError:
 		continue
