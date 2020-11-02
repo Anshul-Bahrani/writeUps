5446e6897fc85ca4de265def7daddabe62c30a0f
Anshul-Bahrani <anshulbahrania@gmail.com>
2020-11-02 18:38:27 +0000

    Scripting 2 on the way

--- tryhackme/scripting/scripting2.py
+++ tryhackme/scripting/scripting2.py
@@ -1,5 +1,11 @@
-import sockets
+import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.connect("10.10.222.145", 3010)
+while True:
+	try:
+		s.connect(("10.10.113.180", 1337))
+		s.send("GET / HTTP/1.0\r\n\r\n".encode())
+		print(s.recv(4096).decode())
+	except ConnectionRefusedError:
+		continue
+	# print(s.recv(4096).decode())
