e8007cf7f457e87ad150b0c255083c93227c4174
root <root@kali>
2020-11-12 11:05:10 -0500

    HTB SneakyMailer on the way

---
+++ hackthebox/sneakymailer/README.md
@@ -0,0 +1,12 @@
+### SneakyMailer ###
+
+export IP=10.10.10.197
+
+
+Edited the /etc/hosts file to get the url up and working...
+
+Found a comment in the source code of the home page...
+
+<!-- need to add Register link to Sidebar for /pypi/register.php -->
+
+Trying to register..on that url and forgot to run nmap and gobuster stuff so doing that..maybe will also run gobuster on /pypi
--- tryhackme/scripting/scripting2.py
+++ tryhackme/scripting/scripting2.py
@@ -1,13 +1,49 @@
 import socket
+import sys
+import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ip = "10.10.70.49"
-port = 3010
-while True:
+ip = "10.10.41.184"
+port = 1337
+answer = 0.00000
+
+def operation(operator, number):
+	global answer
+	print(f"{answer} {operator} {number}")
+	if operator == "add":
+		answer += number
+	elif operator == "minus":
+		answer -= number
+	elif operator == "divide":
+		answer /= number
+	elif operator == "multiply":
+		answer *= number
+	else:
+		print("STOP Found: ",operator)
+		print("Final answer: ", answer)
+		sys.exit(0)
+while port != 9765:
 	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		time.sleep(1)
 		s.connect((ip, port))
-		s.send(f"GET / HTTP/1.0\r\n\r\nHost: {ip}: {port}".encode())
-		print(s.recv(4096).decode())
+		s.send(f"GET / HTTP/1.0\r\n\r\nHost: {ip}:{port}\r\n\r\n".encode())
+		print(f"\nFound {port}: ")
+		fullContent = s.recv(4096).decode()
+		# print(fullContent)
+		lineSplitted = [x for x in fullContent.split("\n")]
+		print(len(lineSplitted))
+		if len(lineSplitted) <= 3:
+			continue
+		mainContent = lineSplitted[6]
+		print("Main:", mainContent)
+		operator, number, port = mainContent.split()
+		port = int(port)
+		operation(operator,float(number))
+		s.close()
 	except ConnectionRefusedError:
+		print(f"Trying {port}...",end="\r")
 		continue
-	# print(s.recv(4096).decode())
+print("\nPort 9765 found final answer: ", answer)
+	# print(s.recv(4096).decode()) Host: {ip}:{port}
+
+# Port 9765 found final answer:  344769.12000000005
